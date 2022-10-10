f0cec1e93872a44c20f689a2c76448bb06137804
Garnier David <david.garnier483@orange.fr>
2022-10-10 18:07:14 +0200

    dev: preparing rolldice component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import Die from './components/Die';
 import RollDice from './components/RollDice';
 import './styles/App.css';
 
 function App() {
 	return (
 		<div className="App">
-			<Die face={4} />
-			<Die face={2} />
+			<RollDice />
 		</div>
 	);
 }
--- src/components/RollDice.js
+++ src/components/RollDice.js
@@ -4,7 +4,36 @@ import React from 'react';
 
 import Die from './Die';
 
-export default function RollDice() {
-	// methode roll()
-	return <i className="fas fa-dice-one" />;
+export default class RollDice extends React.Component {
+	static defaultProps = {
+		sides : [
+			'one',
+			'two',
+			'three',
+			'four',
+			'five',
+			'six'
+		]
+	};
+	constructor(props) {
+		super(props);
+		this.state = {
+			die1 : 'one',
+			die2 : 'one'
+		};
+	}
+	roll() {
+		// pick 2 numbers between 1 and 6
+		const newDie1 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
+	}
+	render() {
+		return (
+			<div>
+				<h1>TEST ROLLDICE Component</h1>
+				<Die face={this.state.die1} />
+				<Die face={this.state.die2} />
+				<button onClick={this.roll}>Lancer les dés !</button>
+			</div>
+		);
+	}
 }
